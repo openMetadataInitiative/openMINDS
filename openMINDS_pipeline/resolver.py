5d79e4a063d0325216113fc500ae72086fc144d5
raphaelgazzotti <raphael.gazzotti@ebrains.eu>
2025-04-09 08:47:18 +0200

    Fix https://github.com/openMetadataInitiative/openMINDS/issues/3

--- openMINDS_pipeline/resolver.py
+++ openMINDS_pipeline/resolver.py
@@ -166,7 +166,7 @@ def _do_resolve_categories(version: str, schema: SchemaStructure, schemas_by_cat
                     if linked_category in schemas_by_category:
                         linked_types.extend(schemas_by_category[linked_category])
                 schema_payload["properties"][p][TEMPLATE_PROPERTY_LINKED_TYPES] = sorted(linked_types)
-                del schema_payload["properties"][p][TEMPLATE_PROPERTY_LINKED_CATEGORIES]
+                schema_payload["properties"][p]["_belongsToCategory"] = schema_payload["properties"][p].pop(TEMPLATE_PROPERTY_LINKED_CATEGORIES)
             if TEMPLATE_PROPERTY_EMBEDDED_CATEGORIES in schema_payload["properties"][p]:
                 embedded_categories = schema_payload["properties"][p][TEMPLATE_PROPERTY_EMBEDDED_CATEGORIES]
                 embedded_types = []
